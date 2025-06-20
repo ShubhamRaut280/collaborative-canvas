981ce6d3d98d5a48981b53fe4db29b12a184e7af
Shubham Raut <124999374+ShubhamRaut280@users.noreply.github.com>
2025-06-20 09:45:20 +0530

    before migrating to redux

---
+++ app/redux/store/store.ts
@@ -0,0 +1,10 @@
+// import {configureStore} from '@reduxjs/toolkit';
+
+// const store = configureStore({
+//   reducer: {
+//     // your reducers go here
+//   }
+// });
+
+// export default store;
+
